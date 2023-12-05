5a5eee03781c97c465819199c55047427f0d2eba
Carlos Eduardo <132010811+Kadudesu@users.noreply.github.com>
2023-12-05 11:47:34 -0300

    btnDelete.jsx

--- src/components/btnCrud/btnDelete.jsx
+++ src/components/btnCrud/btnDelete.jsx
@@ -18,6 +18,7 @@
 //             <button className="BtnDeletar">Deletar</button>
 //           </div>
 //         </div>
+
 //       </div>
 //     </>
 //   );
@@ -25,49 +26,43 @@
 
 // export default BtnDeletar;
 
-import React from "react";
+
+import React, { useState } from "react";
+import HomeLog from "../../pages/homeLog";
 import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import "../btnAdicionar/btnAdicionar.css";
 
-const BtnDeletar = ({ productId }) => {
+const BtnDeletar = ({ itemId }) => {
   const nav = useNavigate();
 
-  const handleDeletar = () => {
-    // Envia a solicitação para deletar o produto pelo ID
-    fetch(`https://funny-handkerchief-newt.cyclic.app/deletar/${productId}`, {
-      method: "DELETE",
-    })
-      .then((resposta) => {
-        if (!resposta.ok) {
-          throw new Error("Erro ao deletar o item");
-        }
-        return resposta.json();
-      })
-      .then(() => {
-        // Faz alguma coisa após o sucesso da deleção
-        toast.success("Item deletado com sucesso!");
-        nav("/logado"); // Navega de volta para a página logado após a deleção
-      })
-      .catch((erro) => {
-        console.error(erro);
-        toast.error("Erro ao deletar o item. Tente novamente mais tarde.");
+  const handleDeletar = async () => {
+    try {
+      const response = await fetch(`https://funny-handkerchief-newt.cyclic.app/deletar/${itemId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': 'https://funny-handkerchief-newt.cyclic.app',
+        },
       });
+
+      if (response.ok) {
+        toast.success("Item deletado com sucesso!");
+        nav("/logado");
+      } else {
+        toast.error("Não foi possível deletar o item.");
+      }
+    } catch (error) {
+      console.error("Erro ao deletar o item:", error);
+      toast.error("Erro ao deletar o item. Tente novamente mais tarde.");
+    }
   };
 
   return (
     <>
+      <HomeLog />
       <div className="popup">
-        <ToastContainer
-          position="bottom-right"
-          autoClose={3000}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          draggable
-          pauseOnHover
-        />
         <div className="container-add">
           <h1>Tem certeza?</h1>
           <div className="btns">
@@ -85,3 +80,53 @@ const BtnDeletar = ({ productId }) => {
 };
 
 export default BtnDeletar;
+
+
+
+// import React, { useState } from "react";
+// import axios from "axios";
+// import { useNavigate } from "react-router-dom";
+// import "../btnAdicionar/btnAdicionar.css";
+// import HomeLog from "../../pages/homeLog";
+
+// const BtnDeletar = ({ id }) => {
+//   const [showPopup, setShowPopup] = useState(false);
+//   const nav = useNavigate();
+
+//   const handleDeletar = () => {
+//     axios
+//       .delete(`https://funny-handkerchief-newt.cyclic.app/deletar/${id}`)
+//       .then(() => {
+//         console.log("Produto deletado com sucesso!");
+//         // Você pode redirecionar para a página desejada após a exclusão
+//         nav("/logado");
+//       })
+//       .catch((error) => {
+//         console.error("Erro ao deletar produto:", error);
+//       });
+//   };
+
+//   return (
+//     <>
+//       <HomeLog />
+//       <div className="popup">
+//         <div className="container-add">
+//           <h1>Tem certeza?</h1>
+//           <div className="btns">
+//             <button
+//               className="BtnCancelar"
+//               onClick={() => nav("/logado")}
+//             >
+//               Cancelar
+//             </button>
+//             <button className="BtnDeletar" onClick={handleDeletar}>
+//               Deletar
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default BtnDeletar;
